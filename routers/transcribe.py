752f33c9bfe4f2a7dc0907b35bd3614e3d200f2d
Degeri <3656517@mail.ru>
2025-07-30 12:21:53 +0300

    new one

--- routers/transcribe.py
+++ routers/transcribe.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
-from services import wisperService
-import os
+from services.wisperService import wisperService
+
 router = APIRouter(
-    prefix = "/function",
+    prefix = "/api",
     tags = ["models"],
     responses = {404 : {"description" : "Not found"}}
 )
 
 @router.get("/transcribe/{file_path:path}")
 async def transcribation(file_path : str):
-    service = WisperService()
+    service = wisperService()
     result = service.transcribe(f"../files/{file_path}")
     return {"text" : result}
--- services/wisperService.py
+++ services/wisperService.py
@@ -3,11 +3,9 @@ import whisper
 import librosa
 
 class wisperService:
-    def __init__(self):
-        self.model = whisper.load_model("tiny").to("cuda")
-
     def transcribe(self, name):
         self.name = name
         audio, sr = librosa.load(self.name, sr=16000)
-        result = self.model.transcribe(audio, language= None, fp16= torch.cuda.is_available())
+        model = whisper.load_model("tiny").to("cuda")
+        result = model.transcribe(audio, language="ru", fp16=torch.cuda.is_available())
         return result["text"]
